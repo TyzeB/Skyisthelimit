469453b0db7c2bcf272ee0623128dbe02b8b6df9
Matic <matic.bozic@protonmail.com>
2020-07-02 02:02:40 +0200

    Added another fetch, modified the HTML

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <div id="app" class="weather">
-    <input id="city-input" type="text" v-model="cityName" />
-    <button id="get-weather">GET</button>
-    <Weather :city="cityName" :temperature="temperature" :description="description" />
+  <div id="app">
+    <div class="weather">
+      <Weather
+        :city="finalCity"
+        :temperature="temperature"
+        :description="description"
+        :image="image"
+      />
+      <div class="search">
+        <input id="city-input" type="text" v-model="cityName" />
+        <button id="get-weather">
+          <i class="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,28 +23,65 @@ import Weather from "./components/Weather.vue";
 export default {
   name: "App",
   components: {
-   Weather
+    Weather
   },
   mounted() {
     const submit = document.querySelector("#get-weather");
     const self = this;
     submit.addEventListener("click", () => {
+      if (!self.cityName) {
+        alert("Please enter city name first");
+        return;
+      }
       self.fetchData(process.env.VUE_APP_API_URL, self.cityName);
+      self.fetchImage(self.cityName);
     });
   },
   data() {
     return {
       cityName: "",
       temperature: "",
-      description: ""
+      description: "",
+      finalCity: "",
+      image: ""
     };
   },
   methods: {
     async fetchData(url, city) {
-      const response = await fetch(`${url}q=${city}&appid=${process.env.VUE_APP_API_KEY}&units=metric`);
-      const data = await response.json();
-      this.temperature = `${parseInt(data.main.temp).toFixed(0)} °C`;
-      this.description = data.weather[0].description.toUpperCase();
+      const self = this;
+      await fetch(`${url}q=${city}&appid=${process.env.VUE_APP_API_KEY}&units=metric`)
+        .then(response => {
+          if (!response.ok) {
+            throw Error("Ooops, something went wrong!");
+          }
+          return response.json();
+        })
+        .then(data => {
+          self.temperature = `${parseInt(data.main.temp).toFixed(0)} °C`;
+          self.description = data.weather[0].description.toUpperCase();
+          self.finalCity = self.cityName;
+        })
+        .catch(error => {
+          alert(error);
+        });
+    },
+    async fetchImage(city) {
+      const self = this;
+      city = city.toLowerCase().replace(/\s+/g, "-");
+      await fetch(`https://api.teleport.org/api/urban_areas/slug:${city}/images/`)
+        .then(response => {
+          if (!response.ok) {
+            throw Error("Ooops, something went wrong!");
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          self.image = data.photos[0].image.web;
+        })
+        .catch(error => {
+          alert(error);
+        });
     }
   }
 };
